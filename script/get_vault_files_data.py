0a8796aad6e235bb4962d1facfa2647b0b1560fc
Kapil Gaire <eriaglipak312@gmail.com>
2026-09-08 17:53:58 +0545

    add: added function to extract vault files path and lines

---
+++ script/get_vault_files_data.py
@@ -0,0 +1,37 @@
+import asyncio
+from pathlib import Path
+
+import aiofiles
+
+LINES_TO_READ = 15
+
+
+async def get_file_path_and_lines(file_path):
+    print(file_path)
+    async with aiofiles.open(file=file_path, mode="r") as f:
+        lines = []
+        for _ in range(LINES_TO_READ):
+            line = await f.readline()
+
+            if not line:
+                break
+            lines.append(line)
+
+    return f" {str(file_path)} =>  {''.join(lines)}"
+
+
+async def get_vault_files_info():
+    """
+    Iterate over all the files in Vault folder ending with .md to extract the file pathe and first 15 lines.
+
+    """
+
+    root_folder = Path("./Vault/")
+
+    tasks = [
+        get_file_path_and_lines(file_path) for file_path in root_folder.rglob("*.md")
+    ]
+
+    results = await asyncio.gather(*tasks)
+
+    return "\n".join(results)
--- script/note_context.py
+++ script/note_context.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 from script.claude_chat_scraper import scrape_claude_chat
+from script.get_vault_files_data import get_vault_files_info
 
 
 @dataclass()
@@ -9,6 +10,9 @@ class NoteContext:
     chat_id: str | None = None
     chat_messages: list[dict[str, str]] = field(default_factory=lambda: [])
     total_message_length: int = 0
+
+    # contains all the note files path and first 15 lines of each file.
+    vault_files_info: str = ""
 
     async def set_chat_context(self):
         messages = await scrape_claude_chat(self.chat_url)
@@ -19,3 +23,7 @@ class NoteContext:
         self.chat_messages = messages
         self.total_message_length = len(messages)
         self.chat_id = self.chat_url.split("/").pop()
+
+    async def set_vault_files_info(self):
+        files_data = await get_vault_files_info()
+        self.vault_files_info = files_data
